50814e19d0c657cecc5b29269fe8b88b2ab5e501
Aliev Omar <omaraliev7729@gmail.com>
2024-02-03 21:04:27 +0200

    1.4-1.6
    create .gitignore + create project C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Введіть перше число");
+double one = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введіть друге число:");
+
+double two = Convert.ToDouble(Console.ReadLine());
+
+double sum = one + two;
+
+Console.WriteLine($"Сумма чисел {one} и {two} равна: {sum}");
+
+Console.ReadKey();
